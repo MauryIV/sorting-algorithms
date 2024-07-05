17cd230490f7762a5d6c25af294b6ad31c04412c
MauryIV <mauryhughesiv@gmail.com>
2024-07-05 13:09:34 -0600

    bubble-sort completed

--- bubble-sort.js
+++ bubble-sort.js
@@ -7,7 +7,18 @@ for (let i = 0; i < 2000; i++) {
 }
 
 // TODO: create bubble sort function
-const bubbleSort = (array) => {};
+const bubbleSort = (array) => {
+  for (let i=0; i<array.length; i++) {
+    for (let j=0; j<array.length -i -1; j++) {
+      if (array[j] > array[j+1]) {
+        let swap = array[j];
+        array[j] = array[j+1];
+        array[j+1] = swap; 
+      }
+    }
+  }
+  return array;
+};
 
 const sorted = bubbleSort(unsortedInputArray);
 console.log('Post Sort:', sorted.join(' '));
