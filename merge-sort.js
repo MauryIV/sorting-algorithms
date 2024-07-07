6e72502bdc4c64640f78a3e00a3fa1a3fdd017a3
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 13:40:31 -0600

    merge-sort completed

--- merge-sort.js
+++ merge-sort.js
@@ -7,7 +7,35 @@ for (let i = 0; i < 2000; i++) {
 }
 
 // TODO: create merge sort function
-const mergeSort = (array) => {};
+const mergeSort = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+  const mid = array.length/2;
+  const firstHalf = array.slice(0, mid);
+  const secondHalf = array.slice(mid, array.length);
+
+  const sortFirstHalf = mergeSort(firstHalf);
+  const sortSecondHalf = mergeSort(secondHalf);
+
+  const merge = (firstArr, secondArr) => {
+    let sortedArray = [];
+    let firstIndex = 0;
+    let secondIndex = 0;
+    while (firstIndex < firstArr.length && secondIndex < secondArr.length) {
+      if (firstArr[firstIndex] < secondArr[secondIndex]) {
+        sortedArray.push(firstArr[firstIndex]);
+        firstIndex++;
+      } else {
+        sortedArray.push(secondArr[secondIndex]);
+        secondIndex++;
+      }
+    }
+    return sortedArray.concat(firstArr.slice(firstIndex)).concat(secondArr.slice(secondIndex));
+  };
+
+  return merge(sortFirstHalf, sortSecondHalf);
+};
 
 const sorted = mergeSort(unsortedInputArray);
 console.log('Post Sort:', sorted.join(' '));
