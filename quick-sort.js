d9aa76140f23feb41f2194330905dde1ce40b644
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 14:08:34 -0600

    quick-sort completed

--- quick-sort.js
+++ quick-sort.js
@@ -6,7 +6,24 @@ for (let i = 0; i < 2000; i++) {
 }
 
 // TODO: create quick sort function
-const quickSort = (array) => {};
+const quickSort = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+
+  const pivot = array[array.length-1];
+  const firstArr = [];
+  const secondArr= [];
+
+  for (let i=0; i<array.length-1; i++) {
+    if (array[i] < pivot) {
+      firstArr.push(array[i]);
+    } else {
+      secondArr.push(array[i]);
+    }
+  }
+  return quickSort(firstArr).concat(pivot, quickSort(secondArr));
+};
 
 const sorted = quickSort(unsortedInputArray);
 console.log('Post Sort:', sorted.join(' '));
